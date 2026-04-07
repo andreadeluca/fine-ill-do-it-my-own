e7c0f806abe96cf314271ca0f51ac6926b79776f
andreadeluca <deluca.andrea29@gmail.com>
2026-04-07 21:58:11 +0200

    Added sliding window algorithm

--- src/web_tools/RateLimiter.py
+++ src/web_tools/RateLimiter.py
@@ -1,26 +1,52 @@
 import datetime
 
+from more_itertools.recipes import sliding_window
+
 CALL_COUNT_DICT_LABEL = "call_count"
 TIMESTAMP_DICT_LABEL = "timestamp"
 
 class RateLimiter:
 
-    def __init__(self, max_calls : int, period: float):
+    def __init__(self, max_calls : int, period: float, sliding_window_mode : bool = False):
         self._max_calls = max_calls
         self._period = period
         self._call_pool = {}
+        self._sliding_window_mode = sliding_window_mode
 
     async def is_allowed(self, call_id : str) -> bool:
-        if call_id in self._call_pool:
-            self._call_pool[call_id][CALL_COUNT_DICT_LABEL] +=1
+        if self._sliding_window_mode:
+            return await self.sliding_window_limiter_check(call_id)
         else:
-            self._call_pool.update({call_id : {CALL_COUNT_DICT_LABEL:1, TIMESTAMP_DICT_LABEL:datetime.datetime.now()}})
+            return await self.default_limiter_check(call_id)
 
-        threshold_exceeded = self._call_pool[call_id][CALL_COUNT_DICT_LABEL] < self._max_calls
-        rate_expiration = self._call_pool[call_id][TIMESTAMP_DICT_LABEL] + datetime.timedelta(milliseconds=self._period)
+    async def sliding_window_limiter_check(self, call_id : str) -> bool:
+
+        if call_id in self._call_pool:
+            self._call_pool.get(call_id).append(datetime.datetime.now())
+        else:
+            self._call_pool.update({call_id: [datetime.datetime.now()]})
+
+        window_count = 0
+
+        for ts in self._call_pool.get(call_id):
+            if datetime.datetime.now() < ts + datetime.timedelta(milliseconds=self._period):
+                window_count += 1
+
+        return window_count < self._max_calls
+
+
+    async def default_limiter_check(self, call_id : str) -> bool:
+        if call_id in self._call_pool:
+            self._call_pool.get(call_id)[CALL_COUNT_DICT_LABEL] += 1
+        else:
+            self._call_pool.update({call_id: {CALL_COUNT_DICT_LABEL: 1, TIMESTAMP_DICT_LABEL: datetime.datetime.now()}})
+
+        threshold_exceeded = self._call_pool.get(call_id)[CALL_COUNT_DICT_LABEL] < self._max_calls
+        rate_expiration = self._call_pool.get(call_id)[TIMESTAMP_DICT_LABEL] + datetime.timedelta(
+            milliseconds=self._period)
 
         if datetime.datetime.now() > rate_expiration:
-            self._call_pool[call_id][CALL_COUNT_DICT_LABEL] = 1
+            self._call_pool.get(call_id)[CALL_COUNT_DICT_LABEL] = 1
             return True
 
         return threshold_exceeded
--- tests/test_limit_rate.py
+++ tests/test_limit_rate.py
@@ -1,3 +1,5 @@
+import time
+
 from src.web_tools.RateLimiter import RateLimiter
 import pytest
 
@@ -8,8 +10,21 @@ async def test_allowed():
 
 @pytest.mark.asyncio
 async def test_blocked():
-    rl = RateLimiter(max_calls=3, period=1000000000)
+    rl = RateLimiter(max_calls=3, period=100000)
     await rl.is_allowed("user1")
+    await rl.is_allowed("user1")
+    await rl.is_allowed("user1")
+    result = await rl.is_allowed("user1")
+    assert result == False
+
+@pytest.mark.asyncio
+async def test_blocked_sliding():
+    rl = RateLimiter(max_calls=3, period=3000, sliding_window_mode=True)
+    await rl.is_allowed("user1")
+    await rl.is_allowed("user1")
+    await rl.is_allowed("user1")
+    await rl.is_allowed("user1")
+    time.sleep(2)
     await rl.is_allowed("user1")
     await rl.is_allowed("user1")
     result = await rl.is_allowed("user1")
